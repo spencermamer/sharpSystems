b4ae878091a1d8a12808e5b3466a2ab80ef09859
spencermamer <spencermamer@gmail.com>
2013-11-25 00:07:59 -0600

    Specie and compartment

--- sharpSystems/Program.cs
+++ sharpSystems/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace sharpSystems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Cosmos sys = new Cosmos("Test");
+            Tag cell = sys.CreateCompartment("Cell", 1.4);
+            Tag A = sys.CreateNewSpecie("A");
+            Tag B = sys.CreateNewSpecie("B");
+            Tag C = sys.CreateNewSpecie("C");
+            sys.PlaceSpecie(A, cell, 10);
+            sys.PlaceSpecie(B, cell, 5);
+            ((Compartment)cell.TaggedComponent).PrintContents();
+            Console.WriteLine("Cell has C?: " + ((Compartment)cell.TaggedComponent).HasSpecie(C));
+
         }
     }
 }
