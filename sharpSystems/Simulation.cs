2114702db55046b9d6b60ce2e0b01519dbc3a85b
spencermamer <spencermamer@gmail.com>
2013-11-27 16:12:54 -0600

    Began creating simulation classes

---
+++ sharpSystems/Simulation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharpSystems
+{
+    public abstract class Simulation
+    {
+        abstract void initialize();
+        abstract void reset();
+        abstract void step();
+    }
+}
