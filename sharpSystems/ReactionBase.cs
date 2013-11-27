887775be5f75acd3cb27e38efa3518d644d7f2ce
spencermamer <spencermamer@gmail.com>
2013-11-27 15:52:23 -0600

    Added some previously unincluded files

---
+++ sharpSystems/ReactionBase.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharpSystems
+{
+    public class ReactionBase : Component
+    {
+        protected enum Role {REACTANT, PRODUCT}
+        protected class Reagent
+        {
+            private Specie specie;
+            public Specie Specie
+            {
+                get { return specie; }
+                private set { specie = value; }
+            }
+            private int stoich;
+            public int Stoich
+            {
+                get { return stoich; }
+                private set { stoich = value; }
+            }
+            private Role reactionRole;
+            public Role ReactionRole
+            {
+                get { return reactionRole; }
+                private set { reactionRole = value; }
+            }
+            public Reagent(Specie specie, int stoich, Role role)
+            {
+                this.specie = specie;
+                this.stoich = stoich;
+                this.reactionRole = role;
+            }
+        }
+
+        public ReactionBase(string name) : base(name) { }
+    }
+}
---
+++ sharpSystems/SpecieTransferReaction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharpSystems
+{
+    public class TransferReaction : Reaction
+    {
+
+        public TransferReaction TransferReactionFactory(string spName, Compartment origin, Compartment location, int stoich=1)
+        {
+            return new TransferReaction("transfer_" + spName, origin.GetSpecie(spName), location.GetSpecie(spName), stoich);
+
+        }
+
+        private TransferReaction(string name, Specie spOrigin, Specie spDest, int stoich)
+            : base(name)
+        {
+            AddProduct(spDest, stoich);
+        }
+    }
+}
