6019a64881dc1b9de624918d784ce57e3aad82ff
spencermamer <spencermamer@gmail.com>
2013-11-28 13:50:14 -0600

    Started creating Simulator Controller and Recorder class hierarchies

---
+++ sharpSystems/Simulator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharpSystems
+{
+    public abstract class Simulator : ISimulator
+    {
+    }
+}
---
+++ sharpSystems/StochasticSimulator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sharpSystems
+{
+    public abstract class StochasticSimulator : Simulator
+    {
+    }
+}
