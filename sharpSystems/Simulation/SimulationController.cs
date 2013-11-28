31f17dfd7e9406c1451b769126055a3baf5346e9
spencermamer <spencermamer@gmail.com>
2013-11-28 00:38:45 -0600

    Create (empty) class ModelWrapper, which will perform the work of taking a Model object and feeding it to a simulator

---
+++ sharpSystems/Simulation/SimulationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace sharpSystems
+{
+    namespace Simulation
+    {
+        public abstract class Controller
+        {
+            public abstract void SendCancelRequest();
+            public abstract void SendStopRequest();
+            public abstract void GetStatus();
+        }
+    }
+}
